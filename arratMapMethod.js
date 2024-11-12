3d99ce915b4fb3136ddbe0419b45ba3e7ba7ef78
sir-serge <irkzsrg@gmail.com>
2024-11-12 21:11:19 +0200

    Create arratMapMethod.js
    learn array.method

---
+++ arratMapMethod.js
@@ -0,0 +1,6 @@
+let mainArray=[1,2,3,4,5,6,7,8,9,90,80,70,60,50,40,30,20,20,10]
+let MappedArray=mainArray.map(callbackFunction);
+function callbackFunction(value,index,arr){
+    return value*index
+}
+console.log(MappedArray);
